e316381d487dd599ffe37b5ed65e529e31822351
avalsonigara15 <avals1504@gmail.com>
2024-02-15 22:24:44 +0530

    Login Design ~Aval

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,13 @@
 // Login.js
 
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { loginUser, registerUser } from '../firebase/authActions'; // Assuming you have actions for Redux
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../firebase/authActions"; // Assuming you have actions for Redux
+import { FaRegEnvelope, FaUserAlt } from "react-icons/fa";
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [isRegistering, setIsRegistering] = useState(false); // State to manage registration form visibility
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const dispatch = useDispatch();
 
   const handleLogin = (e) => {
@@ -16,52 +15,53 @@ const Login = () => {
     dispatch(loginUser(email, password));
   };
 
-  const handleRegister = (e) => {
-    e.preventDefault();
-    if (password === confirmPassword) {
-      registerUser(email, password);
-    } else {
-      alert("Passwords do not match!");
-    }
-  };
-
-  const toggleForm = () => {
-    setIsRegistering(prevState => !prevState);
-  };
-
   return (
-    <div>
-      <h2>{isRegistering ? 'Register' : 'Login'}</h2>
-      <form onSubmit={isRegistering ? handleRegister : handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {isRegistering && (
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        )}
-        <button type="submit">{isRegistering ? 'Register' : 'Login'}</button>
-      </form>
+    <div className="container">
+      <div className="contact-area mg-top-120 mb-120">
+        <div className="row g-0 justify-content-center">
+          <div className="col-lg-7">
+            <form className="contact-form text-center" onSubmit={handleLogin}>
+              <h3>LOGIN</h3>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="single-input-inner">
+                    <label>
+                      <FaUserAlt />
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <div className="single-input-inner">
+                    <label>
+                      <FaRegEnvelope />
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
 
-      {isRegistering && (
-        <p onClick={toggleForm}>Already have an account? Login here</p>
-      )}
+                <div className="col-12">
+                  <button className="btn btn-base" type="submit">
+                    LOGIN
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
