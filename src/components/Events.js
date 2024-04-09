623b65701a76d0fd8d110b190212ddb08ba50826
Rohit Lohar <loharrohit45@gmail.com>
2024-04-09 19:43:26 +0530

    Update

--- src/components/Events.js
+++ src/components/Events.js
@@ -31,38 +31,44 @@ const Events = () => {
 	}, []);
 	return (
 		<>
-			<div className="section-title text-center" style={{ marginBottom: "0", marginTop: "5%" }}>
-				<h4 className="subtitle style-2">Events</h4>
-			</div>
+			{events.length === 0 ? (
+				<p></p>
+			) : (
+				<>
+					<div className="section-title text-center" style={{ marginBottom: "0", marginTop: "5%" }}>
+						<h4 className="subtitle style-2">Events</h4>
+					</div>
 
-			<Grid templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }} gap={10}>
-				{events.map((event, index) => (
-					<Box
-						key={index}
-						borderWidth="1px"
-						borderRadius="lg"
-						overflow="hidden"
-						boxShadow="xl"
-						p={4}
-					>
-						<Image
-							src={event.imageUrl}
-							alt={event.title}
-							width="100%"
-							height="250px"
-							objectFit="cover"
-						/>
-						<Box p={4}>
-							<Text fontSize="xl" fontWeight="bold" mb={2}>
-								{event.title}
-							</Text>
-							<Text fontSize="l" mb={2}>
-								{event.description}
-							</Text>
-						</Box>
-					</Box>
-				))}
-			</Grid>
+					<Grid templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }} gap={10}>
+						{events.map((event, index) => (
+							<Box
+								key={index}
+								borderWidth="1px"
+								borderRadius="lg"
+								overflow="hidden"
+								boxShadow="xl"
+								p={4}
+							>
+								<Image
+									src={event.imageUrl}
+									alt={event.title}
+									width="100%"
+									height="250px"
+									objectFit="cover"
+								/>
+								<Box p={4}>
+									<Text fontSize="xl" fontWeight="bold" mb={2}>
+										{event.title}
+									</Text>
+									<Text fontSize="l" mb={2}>
+										{event.description}
+									</Text>
+								</Box>
+							</Box>
+						))}
+					</Grid>
+				</>
+			)}
 		</>
 	);
 };
