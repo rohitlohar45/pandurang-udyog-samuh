809e4489a0fae8c9e3c0218a7b138f968a360af3
avalsonigara15 <avals1504@gmail.com>
2024-05-02 10:19:57 +0530

    Update ~Aval

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,101 +1,120 @@
 import React, { useState, useEffect } from "react";
-import { Box, SimpleGrid, Text, Image, Heading, Button } from "@chakra-ui/react";
-import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { Box, Grid, Text, Image } from "@chakra-ui/react";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
 import { firestore } from "../firebase/initialise";
 import Pagination from "./Pagination";
 
-const PAGE_SIZE = 9; // Number of awards per page
+const PAGE_SIZE = 6; // Number of awards per page
 
 const Awards = () => {
-	const [awards, setAwards] = useState([]);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [totalAwards, setTotalAwards] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalAwards, setTotalAwards] = useState(0);
+  const [events, setEvents] = useState([]);
 
-	const fetchAwards = async () => {
-		try {
-			const awardsCollectionRef = collection(firestore, "awards");
-			const q = query(awardsCollectionRef, orderBy("rank"), limit(PAGE_SIZE));
-			const querySnapshot = await getDocs(q);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const eventsRef = collection(firestore, "events");
+        const eventsQuery = query(
+          eventsRef,
+          orderBy("createdAt"),
+          limit(PAGE_SIZE),
+          startAfter((currentPage - 1) * PAGE_SIZE)
+        );
+        const querySnapshot = await getDocs(eventsQuery);
 
-			const fetchedAwards = querySnapshot.docs.map((doc) => ({
-				id: doc.id,
-				...doc.data(),
-			}));
-			setAwards(fetchedAwards);
-			setTotalAwards(querySnapshot.size);
-		} catch (error) {
-			console.error("Error fetching awards:", error);
-		}
-	};
+        const eventsData = [];
+        querySnapshot.forEach((doc) => {
+          eventsData.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
 
-	useEffect(() => {
-		fetchAwards();
-	}, [currentPage]);
+        setEvents(eventsData);
+        setTotalAwards(querySnapshot.size);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
 
-	const handleClick = (award) => {
-		localStorage.setItem("selectedAward", JSON.stringify(award));
-		window.location.href = `award-details/${award.slug}`;
-	};
+    fetchData();
+  }, [currentPage]);
 
-	const totalPages = Math.ceil(totalAwards / PAGE_SIZE);
+  const totalPages = Math.ceil(totalAwards / PAGE_SIZE);
 
-	const handleNextPage = () => {
-		if (currentPage < totalPages) {
-			setCurrentPage((prevPage) => prevPage + 1);
-		}
-	};
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
 
-	const handlePrevPage = () => {
-		if (currentPage > 1) {
-			setCurrentPage((prevPage) => prevPage - 1);
-		}
-	};
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
 
-	return (
-		<Box padding={6} textAlign="center" backgroundColor="#f9f9f9">
-			<SimpleGrid
-				columns={{ base: 1, sm: 2, md: 3 }}
-				spacing={8}
-				padding={4}
-				bg="#f1f1f1"
-				borderRadius="md"
-			>
-				{awards.map((award, index) => (
-					<Box
-						key={index}
-						borderWidth="1px"
-						borderRadius="md"
-						overflow="hidden"
-						boxShadow="lg"
-						p={4}
-						bg="#fff"
-						_hover={{
-							transform: "scale(1.05)",
-							transition: "0.3s",
-							backgroundColor: "#f0f0f0",
-						}}
-					>
-						<Image
-							src={award.information.images[0]}
-							alt={award.title}
-							objectFit="contain"
-							width="100%"
-							height="350px" // Consistent height
-						/>
-						<Text fontSize="lg" fontWeight="bold" mt={3} mb={2} color="#555">
-							{award.title}
-						</Text>
-					</Box>
-				))}
-			</SimpleGrid>
-			<Pagination
-				currentPage={currentPage}
-				totalPages={totalPages}
-				handlePrevPage={handlePrevPage}
-				handleNextPage={handleNextPage}
-			/>
-		</Box>
-	);
+  return (
+    <>
+      <div
+        className="section-title text-center"
+        style={{ marginBottom: "0", marginTop: "5%" }}
+      >
+        <h4 className="subtitle style-2">Events</h4>
+      </div>
+      <Box padding={6} textAlign="center" backgroundColor="#f9f9f9">
+        <Grid
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            sm: "repeat(2,1fr)",
+            md: "repeat(3, 1fr)",
+          }}
+          gap={10}
+        >
+          {events.map((event) => (
+            <Box
+              key={event.id}
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              boxShadow="xl"
+              p={4}
+            >
+              <Image
+                src={event.imageUrl}
+                alt={event.title}
+                width="100%"
+                height="250px"
+                objectFit="cover"
+              />
+              <Box p={4}>
+                <Text fontSize="xl" fontWeight="bold" mb={2}>
+                  {event.title}
+                </Text>
+                <Text fontSize="l" mb={2}>
+                  {event.description}
+                </Text>
+              </Box>
+            </Box>
+          ))}
+        </Grid>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handlePrevPage={handlePrevPage}
+          handleNextPage={handleNextPage}
+        />
+      </Box>
+    </>
+  );
 };
 
 export default Awards;
